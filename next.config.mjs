424c10f6f5ad66e7b2cf49af8a13e725c6bb3738
alexjwick <alexjwick03@gmail.com>
2024-03-16 13:47:13 -0400

    fixed basepath by adding env variable

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,7 @@
 /** @type {import('next').NextConfig} */
+
+const isProd = process.env.NODE_ENV === "production";
+
 const nextConfig = {
   /**
    * Enable static exports for the App Router.
@@ -12,7 +15,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  //   basePath: "/ElectionModel2024-site",
+  basePath: isProd ? "/ElectionModel2024-site" : "",
 
   /**
    * Disable server-based image optimization. Next.js does not support

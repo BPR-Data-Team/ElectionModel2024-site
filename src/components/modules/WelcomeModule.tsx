21180ea1f7f46ef8990f263869017135b3768812
alexjwick <alexjwick03@gmail.com>
2024-03-16 13:52:40 -0400

    escaping apostrophes

--- src/components/modules/WelcomeModule.tsx
+++ src/components/modules/WelcomeModule.tsx
@@ -7,12 +7,12 @@ export default function WelcomeModule(): JSX.Element {
         <div className="welcome">
           <h2>Welcome to 24cast</h2>
           <p>
-            This isn't your typical election prediction—we use an all-new method
-            to determine the outcomes of races down to the margin and break down
-            exactly how our model made those conclusions. Produced by the Ivy
-            League's largest political journal, this model takes data across
-            multiple decades and works to explain how each race's history
-            affects expected outcomes.
+            This isn&apos;t your typical election prediction—we use an all-new
+            method to determine the outcomes of races down to the margin and
+            break down exactly how our model made those conclusions. Produced by
+            the Ivy League&apos;s largest political journal, this model takes
+            data across multiple decades and works to explain how each
+            race&apos;s history affects expected outcomes.
           </p>
         </div>
       }

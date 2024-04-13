bef291df9965c7565503f73fb28fd261e0f6da13
alexjwick <alexjwick03@gmail.com>
2024-04-13 16:02:35 -0400

    created some react hooks in the home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,12 +8,18 @@ import ExplainerModule from "@/components/modules/ExplainerModule";
 import SHAPModule from "@/components/modules/SHAPModule";
 import KeyRacesModule from "@/components/modules/KeyRacesModule";
 import SearchModule from "@/components/modules/SearchModule";
+import { useState } from "react";
 
 /**
  * The home page. This is the main page of the site, and is the first page that users see when they visit the site.
  * @returns {JSX.Element} The home page.
  */
 export default function Home(): JSX.Element {
+  const [raceType, setRaceType] = useState<String>("presidential");
+  const [state, setState] = useState<String>("national");
+  const [winner, setWinner] = useState<String>("democrat");
+  const [likelihood, setLikelihood] = useState<number>(0.5);
+  const [margin, setMargin] = useState<number>(0.5);
   return (
     <main className={styles.main}>
       <WelcomeModule />

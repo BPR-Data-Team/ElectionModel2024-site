1cea7b749fb15df07480d462f449d52269584e65
johnlhuangP <john_huang@brown.edu>
2024-04-20 15:00:37 -0400

    issue #4 explainer module fixes

--- src/components/modules/ExplainerModule.tsx
+++ src/components/modules/ExplainerModule.tsx
@@ -8,15 +8,14 @@ export default function ExplainerModule(): JSX.Element {
       <div className={styles.explainer}>
         <h3>How 24cast Predicted This Race</h3>
         <p>
-          We ran <b>10,000</b> simulations predicting election margins using a
+          We ran <span className={styles.boldtext}>10,000</span> simulations predicting election margins using a
           machine learning model trained on data across multiple decades.
         </p>
-        <p>
-          Democrats won in <b>1173</b> simulations and lost 3456.
-        </p>
+        <p>Democrats won in <span className={styles.boldtext}>1173</span> simulations and lost 
+          <span className={styles.boldtext}> 3456</span>.</p>
         <p>
           By running simulations with varied input data, we determined that{" "}
-          <b>economic metrics</b>, <b>polling</b>, and <b>incumbency</b> were
+          <span className={styles.boldtext}>economic metrics</span>, <span className={styles.boldtext}>polling</span>, and <span className={styles.boldtext}>incumbency</span> were
           the most predictive factors in this race.
         </p>
         <p>

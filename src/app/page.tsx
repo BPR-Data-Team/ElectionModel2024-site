19aa618b44e7a71f65d94f6b87df4d7b0066b249
Ariel Shifrin <40341323+arielshif@users.noreply.github.com>
2024-05-04 00:51:03 -0400

    analytics

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -14,6 +14,10 @@ import { State, getStateAbbreviation } from "@/types/State";
 import { Party } from "@/types/Party";
 import { ResponseItem, parseItem } from "@/types/APIResponse";
 import { SHAPFactor } from "@/types/SHAPFactor";
+import ReactGA from 'react-ga'
+
+const TRACKING_ID = "G-QDEM59MHXZ"
+ReactGA.initialize(TRACKING_ID)
 
 interface RaceData {
   winner: Party;

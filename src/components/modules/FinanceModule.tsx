68eb99b03ebd6f674783d2e04aa1d63c67bfde0d
ashlab11 <asherlabovich@gmail.com>
2024-06-30 21:01:41 -0400

    working on campaign finance

---
+++ src/components/modules/FinanceModule.tsx
@@ -0,0 +1,24 @@
+import Module from "../Module";
+import FinanceHist from "@/components/dataviz/Finance";
+import styles from "./FinanceModule.module.css";
+
+
+
+interface FinanceModuleProps {
+    raceType: string;
+    std: number;
+    margins: number[];
+}
+
+const FinanceModule: React.FC<FinanceModuleProps> = ({ raceType, std, margins }) => {
+    return (
+        <Module className="NationalMapModule">
+          <div className={styles.finance}>
+            <h3></h3>
+            <FinanceHist raceType = {raceType} std = {std} margins={margins}/>
+          </div>
+        </Module>
+      );
+}
+
+export default FinanceModule;

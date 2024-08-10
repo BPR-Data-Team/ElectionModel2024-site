3cb9e829ce143833987dc7357e00a6b816c10c75
Ariel Shifrin <40341323+arielshif@users.noreply.github.com>
2024-08-10 12:31:15 -0500

    start

---
+++ src/app/%5Fprivate/wl/demo/page.tsx
@@ -0,0 +1,69 @@
+import styles from "./page.module.css";
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: 'Whitelabel Demo | 24cast.org',
+};
+
+const NewsSite: React.FC = () => {
+  return (
+    <div className={styles.newssite}>
+      <header className={styles.header}>
+      <h1>The Chicago Caller</h1>
+      <div className={styles.navBar}>
+        <span className="material-symbols-outlined">menu</span>
+        <div>
+          <span className="material-symbols-outlined">partly_cloudy_day</span>
+          <p className={styles.weather}>67°F</p>
+        </div>
+        <nav className={styles.nav}>
+          <ul>
+            <li>Local</li>
+            <li>National</li>
+            <li>Politics</li>
+            <li>Opinion</li>
+            <li>2024 Election</li>
+          </ul>
+        </nav>
+        <div>
+          <p className={styles.btn}>Log In</p>
+          <p className={styles.btn}>Subscribe</p>
+          <span className="material-symbols-outlined">search</span>
+        </div>
+        </div>
+      </header>
+      
+      <main className={styles.maincontent}>
+        <section className={styles.mainarticle}>
+          <h2>2024 Election &gt; The Caller Oracle</h2>
+        </section>
+
+        <aside className={styles.sidebar}>
+          <h3>Popular Articles</h3>
+          <ul>
+            {Array(5).fill(0).map((_, i) => (
+              <li key={i}>
+                <div className={styles.articlethumbnail}></div>
+                <p>How 24cast.org predicts elections.<br/><span>Joe Smith</span></p>
+              </li>
+            ))}
+          </ul>
+        </aside>
+
+        <footer className={styles.footer}>
+          <h4>More from The Chicago Caller</h4>
+          <ul className={styles.footerarticles}>
+            {Array(3).fill(0).map((_, i) => (
+              <li key={i}>
+                <div className={styles.articlethumbnail}></div>
+                <p><b>How 24cast.org predicts elections.</b><br/><span>Joe Smith</span></p>
+              </li>
+            ))}
+          </ul>
+        </footer>
+      </main>
+    </div>
+  );
+}
+
+export default NewsSite;

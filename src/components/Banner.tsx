2339e34435b777f0df849888b12f6576fe280051
ashlab11 <asherlabovich@gmail.com>
2024-08-20 23:36:39 -0400

    changed banner again

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,7 +4,7 @@ export default function Banner(): JSX.Element {
   return (
     <div className={styles.banner}>
       <div className={styles.bannerText}>
-        Changes in 24cast.org&apos;s predictions over the last few days are the result{" "} 
+        Changes in 24cast.org&apos;s predictions over the last few days are partially the result{" "} 
         of routine upgrades to our predictive model. <a href="/methodology#changelog">See more.</a> 
       </div>
     </div>

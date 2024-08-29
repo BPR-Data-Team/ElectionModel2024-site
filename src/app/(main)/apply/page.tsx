71adf19573b9c74de836134ac98f660dc41603ed
Ariel Shifrin <40341323+arielshif@users.noreply.github.com>
2024-08-28 21:02:20 -0500

    Update page.tsx

--- src/app/(main)/apply/page.tsx
+++ src/app/(main)/apply/page.tsx
@@ -2,15 +2,13 @@
 import styles from "./page.module.css";
 import type { Metadata } from "next";
 import { useEffect } from 'react';
-import { useRouter } from 'next/navigation';
+import { redirect } from 'next/navigation';
 
 export default function Apply() {
-  const router = useRouter();
 
   useEffect(() => {
-    // Redirect to Google Form
-    window.location.href = 'https://forms.gle/9Ey8wnR7s42Jik657';
-  }, [router]);
+    redirect("https://forms.gle/9Ey8wnR7s42Jik657");
+  }, []);
 
   return (
     <div className={styles.main}>

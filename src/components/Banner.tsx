ca6231cfb6ad3d427e3b48cfc4f2947bd61e3deb
ashlab11 <asherlabovich@gmail.com>
2024-09-11 22:54:14 -0400

    finished latex in log

--- src/components/Banner.tsx
+++ src/components/Banner.tsx
@@ -4,7 +4,7 @@ export default function Banner(): JSX.Element {
   return (
     <div className={styles.banner}>
       <div className={styles.bannerText}>
-        August 24: Changes in our predictions today are the result of the FEC releasing campaign finance data from the month of July.</div>
+        September 11: </div>
     </div>
   );
 }

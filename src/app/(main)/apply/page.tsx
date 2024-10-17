6147e649fc8d9cc401a70c886bb9668cb5f83401
Ariel Shifrin <40341323+arielshif@users.noreply.github.com>
2024-10-17 16:53:45 -0400

    updating apply link

--- src/app/(main)/apply/page.tsx
+++ src/app/(main)/apply/page.tsx
@@ -7,13 +7,13 @@ import { redirect } from 'next/navigation';
 export default function Apply() {
 
   useEffect(() => {
-    redirect("https://forms.gle/9Ey8wnR7s42Jik657");
+    redirect("https://forms.gle/HRd4nGH5JENFUP8FA");
   }, []);
 
   return (
     <div className={styles.main}>
       <p><b>Redirecting to the application form&hellip;</b></p>
-      <p>If you are not redirected, click <a href="https://forms.gle/9Ey8wnR7s42Jik657">here</a>.</p>
+      <p>If you are not redirected, click <a href="https://forms.gle/HRd4nGH5JENFUP8FA">here</a>.</p>
     </div>
   );
 }

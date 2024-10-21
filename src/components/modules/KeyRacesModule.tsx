00a7dd9b50b0591150b44f95cba286da9b8e3792
alexjwick <alexjwick03@gmail.com>
2024-10-21 14:52:32 -0400

    wip on key races module

--- src/components/modules/KeyRacesModule.tsx
+++ src/components/modules/KeyRacesModule.tsx
@@ -3,17 +3,15 @@ import styles from "./KeyRacesModule.module.css";
 import Image from "next/image";
 import bulletPoint from "image-assets/ballot-sequence/ballot-anim00.png";
 import { RaceType } from "@/types/RaceType";
-import {
-  State
-} from "@/types/State";
+import { State } from "@/types/State";
 import { useEffect, useState } from "react";
 
 export const races = [
-  'Washington District 3',
-  'North Carolina Gubernatorial',
-  'New Hampshire Gubernatorial',
-  'Ohio Senate',
-  'Montana Senate'
+  "Washington District 3",
+  "North Carolina Gubernatorial",
+  "New Hampshire Gubernatorial",
+  "Ohio Senate",
+  "Montana Senate",
   // Add more key races as needed
 ];
 
@@ -23,10 +21,11 @@ interface KeyRacesModuleProps {
   setDistrict: (district: number) => void;
 }
 
-export default function KeyRacesModule(props: KeyRacesModuleProps): JSX.Element {
-
-  function handleClick(race: string){
-    if (race === races[0]){
+export default function KeyRacesModule(
+  props: KeyRacesModuleProps
+): JSX.Element {
+  function handleClick(race: string) {
+    if (race === races[0]) {
       props.setRaceType(RaceType.House);
       props.setState(State.Washington);
       props.setDistrict(3);
@@ -59,18 +58,18 @@ export default function KeyRacesModule(props: KeyRacesModuleProps): JSX.Element 
         <h3 className={styles.exploreText}>Explore Key Races:</h3>
         <div className={styles.grid}>
           {races.map((race, index) => (
-            <div key={index} className={styles.race} onClick={() => handleClick(race)}>
-              <Image
-                src={bulletPoint}
-                width={15}
-                alt="bullet point"
-              />
-              <a href="javascript:;" className={styles.linkText}>{race}</a>
-            </div>
+            <a
+              href="#"
+              key={index}
+              className={styles.race}
+              onClick={() => handleClick(race)}
+            >
+              <Image src={bulletPoint} width={15} alt="bullet point" />
+              <span className={styles.raceText}>{race}</span>
+            </a>
           ))}
         </div>
       </div>
     </Module>
   );
-
 }
